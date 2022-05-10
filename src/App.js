6654f928df755b2ac1a6042937cb0e734f98a724
KostiantynKorzh <kostyantynkorzh@gmail.com>
2022-05-10 22:34:01 +0300

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from "react-router-dom";
+import HomePage from "./components/pages/HomePage";
+import CataloguePage from "./components/pages/CataloguePage";
+import Header from "./components/Header";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            {/*штука чтобы чекать, какой путь стоит в браущере и перенаправлять соответствующе*/}
+            {/*http://localhost:3000/ - значит HomePage*/}
+            {/*http://localhost:3000/catalogur - значит CataloguePage*/}
+            <BrowserRouter>
+                {/*сюда ебанули хедер, шоб он был на всех страницах*/}
+                <Header/>
+                <Routes>
+                    <Route path="/" element={<HomePage/>}/>
+                    <Route path="/catalogue" element={<CataloguePage/>}/>
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
