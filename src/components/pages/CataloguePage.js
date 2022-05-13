7b519b2892049436867f77a52ac308efd1436f8f
okgoogle3 <halalah.han@gmail.com>
2022-05-13 20:39:27 +0300

    хуета работает

--- src/components/pages/CataloguePage.js
+++ src/components/pages/CataloguePage.js
@@ -1,14 +1,22 @@
 import React, { useEffect, useState } from 'react';
 
 import ProductService from "../../services/ProductService";
-import {Divider, List, ListItem, ListItemText} from "@mui/material";
+import {
+    Box,
+    Button,
+    Card,
+    CardActions, CardContent,
+    CardMedia,
+    Grid,
+    Typography
+} from "@mui/material";
 
 export const CataloguePage = () => {
     const [ products, setProducts ] = useState([]);
     const getAllProducts = () => {
         ProductService.getAllProducts()
             .then(response => {
-                setProducts(response.data)
+                setProducts(response.data.products)
             })
             .catch(err => {
                 console.log(err)
@@ -20,16 +28,33 @@ export const CataloguePage = () => {
     return (
         <>
             <h3>Products</h3>
-            <List>
+            <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2}>
                 {products && products.map(product => (
-                    <>
-                        <ListItem>
-                            <ListItemText>{product.title}</ListItemText>
-                        </ListItem>
-                        <Divider/>
-                    </>
+                    <Grid Card xs={3}>
+                    <Card sx={{ width: 345, height: 400}}>
+                        <CardMedia
+                            component="img"
+                            height="260"
+                            image={product.thumbnail}
+                            alt={product.title}
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                {product.title}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                {product.description}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small">Add to cart</Button>
+                        </CardActions>
+                    </Card>
+                    </Grid>
                 ))}
-            </List>
+            </Grid>
+            </Box>
         </>
     );
 };
