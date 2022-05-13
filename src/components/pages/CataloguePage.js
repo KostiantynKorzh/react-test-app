163b8f6fd48edccb9a4545c748bd94621b6d41b7
okgoogle3 <halalah.han@gmail.com>
2022-05-13 19:29:26 +0300

    хуета не работает

--- src/components/pages/CataloguePage.js
+++ src/components/pages/CataloguePage.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import ProductService from "../../services/ProductService";
+import {Divider, List, ListItem, ListItemText} from "@mui/material";
 
 export const CataloguePage = () => {
+    const [ products, setProducts ] = useState([]);
+    const getAllProducts = () => {
+        ProductService.getAllProducts()
+            .then(response => {
+                setProducts(response.data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+    useEffect(() => {
+        getAllProducts()
+    }, [])
     return (
         <>
-
+            <h3>Products</h3>
+            <List>
+                {products && products.map(product => (
+                    <>
+                        <ListItem>
+                            <ListItemText>{product.title}</ListItemText>
+                        </ListItem>
+                        <Divider/>
+                    </>
+                ))}
+            </List>
         </>
     );
 };
